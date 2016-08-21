7ce0f6897e9682f09b48a9cc9773abe538cf648c
Rick <rickb777@github.com>
2016-08-21 11:43:45 +0100

    more docs

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 
 Serve static files from a Go http server, including performance-enhancing features.
 
+ * Based on the standard net/http ServeFiles, with gzip and cache performance enhancements.
+
+Please see the [GoDoc](https://godoc.org/github.com/rickb777/servefiles) for more.
+
 ## Licence
 
 [MIT](LICENSE)
--- doc.go
+++ doc.go
@@ -1,41 +1,51 @@
 /*
   Package `servefiles` provides a static asset handler for serving files such as images, stylesheets and
-  javascript code. Care is taken to set headers such that the assets will be efficiently cached by browsers and proxies.
+  javascript code. This is an enhancement to the standard net/http ServeFiles, which is used internally.
+  Care is taken to set headers such that the assets will be efficiently cached by browsers and proxies.
 
       assets := servefiles.AssetHandler(0, "./assets/", time.Hour)
 
   Assets is an http.Handler and can be used alongside your other handlers.
 
+
   Gzipping
 
+  The Assets handler serves gzipped content when the browser indicates it can accept it. But it does not
+  gzip anything on-the-fly. Nor does it create any gzipped files for you.
+
   During the preparation of your web assets, all text files (CSS, JS etc) should be accompanied by their gzipped
-  equivalent. The Assets handler will first check for the gzipped file, which it will serve if present. Otherwise
+  equivalent. The Assets handler will first look for the gzipped file, which it will serve if present. Otherwise
   it will serve the 'normal' file.
 
   This has many benefits: fewer bytes are read from the disk, a smaller memory footprint is needed in the server,
-  fewer bytes are sent across the network, etc.
+  less data copying happens, fewer bytes are sent across the network, etc.
 
-  You should not attempt to gzip already-compressed files, such as JPEG, SVGZ, etc.
+  You should not attempt to gzip already-compressed files, such as PNG, JPEG, SVGZ, etc.
 
   Very small files gain little from compression because they may be small enough to fit within a single TCP packet,
-  so don't bother with them. (They might even grow in size.)
+  so don't bother with them. (They might even grow in size when gzipped.)
+
 
   Cache Control
 
-  The 'far-future' technique can and should be used. Set a long expiry time, e.g. time.Hour * 24 * 3650
+  The 'far-future' technique can and should be used. Set a long expiry time, e.g. time.Hour * 24 * 365 * 10
 
-  No in-memory caching is performed server-side. This is less necessary due to far-future caching being
+  Browsers will cache such assets and not make requests for them for the next ten years (or whatever). Not even
+  conditional requests are made. There is clearly a big benefit in page load times after the first visit.
+
+  No in-memory caching is performed server-side. This is less needed due to far-future caching being
   supported, but might be added in future.
+
 
   Example Usage
 
   Choose any expiry age you like, although ten years seems to work well.
   To serve files with a ten-year expiry, this creates a suitably-configured handler:
 
-      assets := servefiles.AssetHandler(1, "./assets/", 10 * 365 * 24 * time.Hour)
+      assets := servefiles.AssetHandler( 1, "./assets/", 10 * 365 * 24 * time.Hour )
 
-  Notice the first parameter is 1 instead of the default, 0. So the first segment of the URL path is discarded.
-  This means, for example, that the URL
+  Notice here the first parameter is 1 instead of the default, 0. So the first segment of the URL path gets
+  discarded. This means, for example, that the URL
 
       http://example.com/e3b1cf/css/style1.css
 
@@ -44,9 +54,9 @@
       ./assets/css/style1.css
       ./assets/css/style1.css.gz
 
-  without the e3b1cf segment. The benefit of this is that you can use a unique number in that segment (chosen for
-  example each time your server starts). Each time that number changes, browsers will see the asset files as
-  being new, and later will drop old versions from their cache regardless of their ten-year lifespan.
+  without the e3b1cf segment. The benefit of this is that you can use a unique number or hash in that segment (chosen
+  for example each time your server starts). Each time that number changes, browsers will see the asset files as
+  being new, and they will later drop old versions from their cache regardless of their ten-year lifespan.
 
   So you get the ten-year lifespan combined with being able to push out changed assets when you need to.
 */
